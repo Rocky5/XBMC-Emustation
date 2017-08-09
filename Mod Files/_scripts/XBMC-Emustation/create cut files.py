1cc1d4bd1ef577383e0bd54d32dfa2331f28de2b
John <Rocky5@users.noreply.github.com>
2017-08-09 06:37:05 +0100

    1) Updated the xbe and source to not show the .xpr on the end of the label for Skin.CurrentTheme, so now shows carbon instead of carbon.xpr eg.... ( had to update all the xmls that used the stringcompare )
    2) Added logging to the "create cut files.py" script, disabled by default as the auto mode will spam the living hell out of your log file.
    
    3) Simplified the Home.xml backgrounds and labels.
    
    4) Fixed the carousel bar colours, I had them swapped round in the colours xml.
    
    5) New Customize Home Settings menu, with a preview system!
    
    6) Probably other changes :/

--- Mod Files/_scripts/XBMC-Emustation/create cut files.py
+++ Mod Files/_scripts/XBMC-Emustation/create cut files.py
@@ -39,31 +39,42 @@ with open( xbmc.translatePath( "special://xbmc/system/" ) + "xbmc.log", "r" ) as
 			Content_List_Path	= xbmc.translatePath( "Special://skin/720p/content lists/" )
 			Extensions			= [ "zip","bin","img","iso","rom","n64","z64","smd","smc","gb","gbc","gba","nes","sms","swc","gg","a26","a78","col","lnx","sfc","sg","fig","vms","exe" ]
 
+def log( input ):
+	if logging: print "%s" % str( input )
+			
 def manual_scan():
 	Found_Roms = 0
 	Emu_Path = dialog.browse( 0,"Select a Emulator folder",'files','',False,False,Emulator_Path )
 	
+	log('|	Check if _emulators directory is selected instead of the emulator its self.')
 	if Emu_Path == Emulator_Path: return
-	
+		
+	log('|	Convert Q:\\ to XBMCs internal special protocol')
 	if Emu_Path.startswith("Q:\\"): Emu_Path = Emu_Path.replace( "Q:\\", Root_Directory )
-	
+
+	log('|	Checking to make sure the emulator you selected exists.')
 	if os.path.isdir( Emu_Path ):
-	
+		
+		log('|	Set the Countlist variable and set the emu_name variable.')
 		CountList = 1
 		Emu_Name = os.path.split(os.path.dirname( Emu_Path ))[1]
 		
-		if os.path.isfile( Content_List_Path + Emu_Name + '.xml' ): os.remove( Content_List_Path + Emu_Name + '.xml' )
-		
-		with open( Content_List_Path + Emu_Name + '.xml', "w") as outputmenufile:
-			WriteMenuFile = menu_entry_header
-			outputmenufile.write( WriteMenuFile )
-			
+		log('|	Check for a default .xbe in the emullator path you selected.')
 		if os.path.isfile( os.path.join( Emu_Path, "default.xbe" ) ):
 			Emu_XBE = os.path.join( Emu_Path, "default.xbe" )
 		else:
 			dialog.ok("Error","No default.xbe found in this directory")
 			return manual_scan()
-			
+		
+		log('|	Check for previous layout xml and if it exists remove it.')
+		if os.path.isfile( Content_List_Path + Emu_Name + '.xml' ): os.remove( Content_List_Path + Emu_Name + '.xml' )
+
+		log('|	Write new layout xml header.')
+		with open( Content_List_Path + Emu_Name + '.xml', "w") as outputmenufile:
+			WriteMenuFile = menu_entry_header
+			outputmenufile.write( WriteMenuFile )
+		
+		log('|	Check to see if the folder you selected it fba, or mame as these emulators must have there roms in there own roms folder in the emulators root directory')
 		if Emu_Name == "fba":
 			Roms_Folder	= Emulator_Path + '\\fba\\roms\\'
 			Parse_FBL_XML = 1
@@ -72,46 +83,57 @@ def manual_scan():
 		else:
 			Roms_Folder	= dialog.browse( 0,"Select the Roms folder","files",'',False,False,Roms_Path + Emu_Name )
 			Parse_FBL_XML = 0
-			
+		
+		log('|	Convert Q:\\ to XBMCs internal special protocol')
 		if Roms_Folder.startswith("Q:\\"): Roms_Folder = Roms_Folder.replace( "Q:\\", Root_Directory )
 		
+		log('|	Couple more vars being set')
 		Output_Path = os.path.join( CUTFile_Path, Emu_Name ) + '\\'
 		TBN_File = os.path.join( TBN_Path, Emu_Name ) + '\\'
 		
+		log('|	Check to see if the emulators = rom folder is empty and exit if it is.')
 		if len(os.listdir( Roms_Folder )) > 0:
 			
+			log('|	Listing the content of the roms folder for parsing.')
 			for Items in sorted( os.listdir( Roms_Folder ) ):
 
+					log('|	Checking the file I find, extension agains my table.')
 					if Items.endswith(tuple(Extensions)):
-					
+						
+						log('|	More vars being set.')
 						Rom_Name = Items
 						Rom_Name_noext = Rom_Name[:-4]
 						Rom_Path = os.path.join( Roms_Folder, Rom_Name )
 
+						log('|	Check if fba was found and parse its xml files to get the correct rom names for the list.')
 						if Parse_FBL_XML == 1:
 							if os.path.isfile( os.path.join( Emulator_Path, "fba\\info\\emulation\\" ) + Rom_Name_noext + ".ini" ):
 								with open( os.path.join( Emulator_Path, "fba\\info\\emulation\\" ) + Rom_Name_noext + ".ini", 'r') as ini:
 									FBA_Rom_Name = ini.readline()[:-1]
 							else:
 								FBA_Rom_Name = Rom_Name_noext
-								
-						if ( pDialog.iscanceled() ): return
 						
+						log('|	Check to see if vars are the value I need and create a new dialog.')
 						if CountList == 1 and Found_Roms == 0: pDialog.create( "Scanning for Roms","","Please wait..." )
 						
+						log('|	Setting a var again :/')
 						Found_Roms = 1
 						
+						log('|	Check to see if _tbns folder exists and if it doesnt create it.')
 						if not os.path.isdir( TBN_File ): os.makedirs( TBN_File )
 						
+						log('|	Check to see if the output directory exists and remove it.')
 						if CountList == 1 and os.path.isdir( Output_Path ):
-						
 							pDialog.update( 0,"","Doing house cleaning" )
 							shutil.rmtree( Output_Path )# remove old cut files
-							
+						
+						log('|	Create a new output directory.')
 						if not os.path.isdir( Output_Path ): os.makedirs( Output_Path )
 						
+						log('|	Show the progress bar progress.')
 						pDialog.update( ( CountList * 100 ) / len( os.listdir( Roms_Folder ) ),"Processing Roms",Rom_Name_noext,"Please wait..." )
 						
+						log('|	Create the rest of the layout xml file.')
 						with open( Content_List_Path + Emu_Name + '.xml', "a") as outputmenufile:
 							if Emu_Name == "fba":
 								WriteMenuFile = menu_entry % (CountList,FBA_Rom_Name,'Runxbe( "' + Output_Path + Rom_Name_noext + '.cut" )',TBN_File + Rom_Name_noext + '.tbn',TBN_File + Rom_Name_noext + '.tbn')
@@ -120,7 +142,8 @@ def manual_scan():
 							else:
 								WriteMenuFile = menu_entry % (CountList,Rom_Name_noext,'Runxbe( "' + Output_Path + Rom_Name_noext + '.cut" )',TBN_File + Rom_Name_noext + '.tbn',TBN_File + Rom_Name_noext + '.tbn')
 							outputmenufile.write( WriteMenuFile )
-							
+
+						log('|	Create the cut file for this rom.')
 						with open(Output_Path + Rom_Name_noext + '.cut', "w") as outputfile:
 							if Emu_Name == "fba":
 								WriteFile = CUT_File_Layout % ( Emu_XBE,FBA_Rom_Name,Rom_Name_noext )
@@ -129,54 +152,73 @@ def manual_scan():
 							else:
 								WriteFile = CUT_File_Layout % ( Emu_XBE,Rom_Name_noext,Rom_Path )
 							outputfile.write( WriteFile )
-							
+						
+						log('|	Add 1 to the Countlist.')
 						CountList = CountList + 1
 		else:
+			log('|	No roms exist so do some cleanup.')
 			shutil.rmtree( Output_Path )# remove old cut files
 			if not os.path.isdir( Output_Path ): os.makedirs( Output_Path )
-					
+	
+	log('|	Add the footer to the layout xml file.')
 	with open( Content_List_Path + Emu_Name + '.xml', "a") as outputmenufile:
 		WriteMenuFile = menu_entry_footer
 		outputmenufile.write( WriteMenuFile )
 		
+	log('|	Set a property so I can run the next script without this one running on.')
 	if ( Found_Roms == 1 ):
 		xbmcgui.Window(xbmcgui.getCurrentWindowId()).setProperty("MyScript.ExternalRunning", "True")
+		
+		log('|	Running the scan script to update the counters.')
 		xbmc.executebuiltin('RunScript(' + Scripts_Path + 'scan.py,scan_emus,scan_cuts,0)' )
 		
+		log('|	Loop.')
 		while (xbmcgui.Window(xbmcgui.getCurrentWindowId()).getProperty("MyScript.ExternalRunning") == "True"):
 			time.sleep(0.2)
 
+		log('|	Run the create layouts script to refresh them.')
 		xbmc.executebuiltin('RunScript(' + Scripts_Path + 'create layouts.py)' )
 	pDialog.close()
 	return
+
 
 def full_scan(): ## not working yet
 
 	Found_Roms = 0
 	Parse_FBL_XML = 0
 	
+	log('|	Check if _emulators directory is selected instead of the emulator its self.')
 	if os.path.isdir( Emulator_Path ):
 	
+		log('|	Convert Q:\\ to XBMCs internal special protocol')
 		for Emu_Path in sorted( os.listdir( Emulator_Path ) ):
 		
+			log('|	Set the Countlist variable.')
 			CountList = 1
 			
+			log('|	Checking to make sure the emulator you selected exists.')
 			if os.path.isdir( os.path.join( Emulator_Path, Emu_Path ) ):
 			
+				log('|	Set emu_name/path variable.')
 				Emu_Path = os.path.join( Emulator_Path, Emu_Path ) + '\\'
 				Emu_Name = os.path.split(os.path.dirname( Emu_Path ))[1]
 				
+				log('|	Check for previous layout xml and if it exists remove it.')
 				if os.path.isfile( Content_List_Path + Emu_Name + '.xml' ): os.remove( Content_List_Path + Emu_Name + '.xml' )
 				
+				log('|	Write new layout xml header.')
 				with open( Content_List_Path + Emu_Name + '.xml', "w") as outputmenufile:
 				
 					WriteMenuFile = menu_entry_header
 					outputmenufile.write( WriteMenuFile )
-					
+				
+				log('|	Check to see if a defualt.xbe exists')
 				if os.path.isfile( os.path.join( Emu_Path, "default.xbe" ) ):
 				
+					log('|	default.xbe does exist so set the oath varable')
 					Emu_XBE = os.path.join( Emu_Path, "default.xbe" )
 				
+					log('|	Check to see if the current directory is fba or mame and set the roms path.')
 					if Emu_Name == "fba":
 						Roms_Folder	= Emulator_Path + '\\fba\\roms\\'
 						Parse_FBL_XML = 1
@@ -185,44 +227,56 @@ def full_scan(): ## not working yet
 					else:
 						Roms_Folder	= Roms_Path + Emu_Name
 						
+					log('|	Convert Q:\\ to XBMCs internal special protocol')
 					if Roms_Folder.startswith("Q:\\"): Roms_Folder = Roms_Folder.replace( "Q:\\", Root_Directory )
 					
+					log('|	Couple more vars being set')
 					Output_Path = os.path.join( CUTFile_Path, Emu_Name ) + '\\'
 					TBN_File = os.path.join( TBN_Path, Emu_Name ) + '\\'
 					
+					log('|	Check to see if the emulators = rom folder is empty and exit if it is.')
 					if len(os.listdir( Roms_Folder )) > 0:
 					
+						log('|	Listing the content of the roms folder for parsing.')
 						for Items in sorted( os.listdir( Roms_Folder ) ):
 						
+							log('|	Checking the file I find, extension agains my table.')
 							if Items.endswith(tuple(Extensions)):
 							
+								log('|	More vars being set.')
 								Rom_Name = Items
 								Rom_Name_noext = Rom_Name[:-4]
 								Rom_Path = os.path.join( Roms_Folder, Rom_Name )
 								
+								log('|	Check if fba was found and parse its xml files to get the correct rom names for the list.')
 								if Parse_FBL_XML == 1:
-								
 									if os.path.isfile( os.path.join( Emulator_Path, "fba\\info\\emulation\\" ) + Rom_Name_noext + ".ini" ):
 										with open( os.path.join( Emulator_Path, "fba\\info\\emulation\\" ) + Rom_Name_noext + ".ini", 'r') as ini:
 											FBA_Rom_Name = ini.readline()[:-1]
 									else:
 										FBA_Rom_Name = Rom_Name_noext
 										
+									log('|	Check to see if vars are the value I need and create a new dialog.')
 									if CountList == 1 and Found_Roms == 0: pDialog.create( "Scanning for Roms","","Please wait..." )
 									
+									log('|	Setting a var again :/')
 									Found_Roms = 1
 
+									log('|	Check to see if _tbns folder exists and if it doesnt create it.')
 									if not os.path.isdir( TBN_File ): os.makedirs( TBN_File )
 
+									log('|	Check to see if the output directory exists and remove it.')
 									if CountList == 1 and os.path.isdir( Output_Path ):
-									
 										pDialog.update( 0,"","Doing house cleaning" )
 										shutil.rmtree( Output_Path )# remove old cut files
 					
+									log('|	Create a new output directory.')
 									if not os.path.isdir( Output_Path ): os.makedirs( Output_Path )
 									
+									log('|	Show the progress bar progress.')
 									pDialog.update( ( CountList * 100 ) / len( os.listdir( Roms_Folder ) ),"Processing [B][UPPERCASE]" + Emu_Name + "[/UPPERCASE][/B] Roms",Rom_Name_noext,"Please wait..." )
 									
+									log('|	Create the rest of the layout xml file.')
 									with open( Content_List_Path + Emu_Name + '.xml', "a") as outputmenufile:
 										if Emu_Name == "fba":
 											WriteMenuFile = menu_entry % (CountList,FBA_Rom_Name,'Runxbe( "' + Output_Path + Rom_Name_noext + '.cut" )',TBN_File + Rom_Name_noext + '.tbn',TBN_File + Rom_Name_noext + '.tbn')
@@ -232,6 +286,7 @@ def full_scan(): ## not working yet
 											WriteMenuFile = menu_entry % (CountList,Rom_Name_noext,'Runxbe( "' + Output_Path + Rom_Name_noext + '.cut" )',TBN_File + Rom_Name_noext + '.tbn',TBN_File + Rom_Name_noext + '.tbn')
 										outputmenufile.write( WriteMenuFile )
 									
+									log('|	Create the cut file for this rom.')
 									with open(Output_Path + Rom_Name_noext + '.cut', "w") as outputfile:
 										if Emu_Name == "fba":
 											WriteFile = CUT_File_Layout % ( Emu_XBE,FBA_Rom_Name,Rom_Name_noext )
@@ -240,23 +295,32 @@ def full_scan(): ## not working yet
 										else:
 											WriteFile = CUT_File_Layout % ( Emu_XBE,Rom_Name_noext,Rom_Path )
 										outputfile.write( WriteFile )
+									
+									log('|	Add 1 to the Countlist.')
 									CountList = CountList + 1
 					
 					else:
+						log('|	No roms exist so do some cleanup.')
 						shutil.rmtree( Output_Path )# remove old cut files
 						if not os.path.isdir( Output_Path ): os.makedirs( Output_Path )
 						
+			log('|	Add the footer to the layout xml file.')
 			with open( Content_List_Path + Emu_Name + '.xml', "a") as outputmenufile:
 				WriteMenuFile = menu_entry_footer
 				outputmenufile.write( WriteMenuFile )
-				
+	
+	log('|	Set a property so I can run the next script without this one running on.')			
 	if ( Found_Roms == 1 ):
 		xbmcgui.Window(xbmcgui.getCurrentWindowId()).setProperty("MyScript.ExternalRunning", "True")
+		
+		log('|	Running the scan script to update the counters.')
 		xbmc.executebuiltin('RunScript(' + Scripts_Path + 'scan.py,scan_emus,scan_cuts,scan_xbes)' )
 		
+		log('|	Loop.')
 		while (xbmcgui.Window(xbmcgui.getCurrentWindowId()).getProperty("MyScript.ExternalRunning") == "True"):
 			time.sleep(0.2)
 
+		log('|	Run the create layouts script to refresh them.')
 		xbmc.executebuiltin('RunScript(' + Scripts_Path + 'create layouts.py)' )
 	pDialog.close()
 	return
@@ -280,8 +344,9 @@ menu_entry			= '\n\
 	</item>\n\
 '
 menu_entry_footer	= '</content>'
-if not os.path.isdir( Content_List_Path + 'merged' ): os.makedirs( Content_List_Path + 'merged' )
 
+logging = 0 # Setting this to 1 will spam the living hell out of your log file if you run the Auto mode, you have been warned
+if not os.path.isdir( Content_List_Path + 'merged' ): os.makedirs( Content_List_Path + 'merged' )
 if Manual_Scan == "manual" : manual_scan()
 if Full_Scan == "auto" : full_scan()
 
---
+++ New XBMC xbe/Source Edits/xbmc/Shortcut.h
@@ -0,0 +1,51 @@
+/*
+ *      Copyright (C) 2005-2013 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+// Shortcut.h: interface for the CShortcut class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_SHORTCUT_H__641CCF68_6D2A_426E_9204_C0E4BEF12D00__INCLUDED_)
+#define AFX_SHORTCUT_H__641CCF68_6D2A_426E_9204_C0E4BEF12D00__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "utils/StdString.h"
+
+class CShortcut
+{
+public:
+  CShortcut();
+  virtual ~CShortcut();
+
+  bool Create(const CStdString& szPath);
+  bool Save(const CStdString& strFileName);
+
+  CStdString m_strPath;
+  CStdString m_strVideo;
+  CStdString m_strParameters;
+  CStdString m_strCustomGame;
+  CStdString m_strThumb;
+  CStdString m_strLabel;
+  CStdString m_strSynopsis;
+};
+
+#endif // !defined(AFX_SHORTCUT_H__641CCF68_6D2A_426E_9204_C0E4BEF12D00__INCLUDED_)
