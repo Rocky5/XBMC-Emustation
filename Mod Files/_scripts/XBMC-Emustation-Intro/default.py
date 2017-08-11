2ccd939243f4bf096c3a32d9e8e1fe6b13eead58
John <Rocky5@users.noreply.github.com>
2017-08-11 02:02:03 +0100

    5) Updated the way intros work, you can now select the file within the UI Settings menu.
    6) Updated the labels in the Quit menu.
    
    7) New font.xml, now has font sizes from 1 to 80

--- Mod Files/_scripts/XBMC-Emustation-Intro/default.py
+++ Mod Files/_scripts/XBMC-Emustation-Intro/default.py
@@ -2,45 +2,38 @@ import os, time, xbmc, xbmcgui
 
 ## Updated by Rocky5 to add dynamic extensions
 
-try:
-	filename	= sys.argv[1]
-except:
-	filename	= ""
+filename = xbmc.getInfoLabel( 'Skin.String(selectedintrofile)' )
 	
 Extensions		= [ "avi","mp4","wmv","xmv" ]
 intro_found = 0
 
-for Items in sorted( os.listdir( xbmc.translatePath( 'Special://xbmc/' ) ) ):
-	if Items.endswith(tuple(Extensions)):
-		if Items.startswith(filename):
-			filename = Items
-			isplayed = xbmc.getInfoLabel( "Window(Home).Property(intro.isplayed)" ).lower() == "true"
-			winPrograms = xbmc.getCondVisibility( 'Window.IsVisible(Programs)' )
+if filename.endswith(tuple(Extensions)):
+	isplayed = xbmc.getInfoLabel( "Window(Home).Property(intro.isplayed)" ).lower() == "true"
+	winPrograms = xbmc.getCondVisibility( 'Window.IsVisible(Programs)' )
 
-			if not isplayed or winPrograms:
-				ReplaceWindowHome = not winPrograms
-				print "XBMC Intro Movie"
+	if not isplayed or winPrograms:
+		ReplaceWindowHome = not winPrograms
+		print "XBMC Intro Movie"
 
-				intro = xbmc.translatePath( 'Special://xbmc/' + filename )
-				if filename.endswith("xmv") or filename.endswith("mp4"):
-					print "dvdplayer"
-					player = xbmc.Player( xbmc.PLAYER_CORE_DVDPLAYER )
-				else:
-					print "mplayer"
-					player = xbmc.Player( xbmc.PLAYER_CORE_MPLAYER )
-				player.play( intro )
-				xbmcgui.Window( 10000 ).setProperty( "intro.isplayed", "true" )
+		if filename.endswith("xmv") or filename.endswith("mp4"):
+			print "dvdplayer"
+			player = xbmc.Player( xbmc.PLAYER_CORE_DVDPLAYER )
+		else:
+			print "mplayer"
+			player = xbmc.Player( xbmc.PLAYER_CORE_MPLAYER )
+		player.play( filename )
+		xbmcgui.Window( 10000 ).setProperty( "intro.isplayed", "true" )
 
-				if ReplaceWindowHome:
-					xbmc.sleep( 500 )
+		if ReplaceWindowHome:
+			xbmc.sleep( 500 )
 
-					while player.isPlaying():
-						#continue
-						time.sleep( .2 )
+			while player.isPlaying():
+				#continue
+				time.sleep( .2 )
 
-					xbmc.executebuiltin( "ReplaceWindow(Home)" )
+			xbmc.executebuiltin( "ReplaceWindow(Home)" )
 
-					xbmc.sleep( 1000 )
-					intro_found = 1
+			xbmc.sleep( 1000 )
+			intro_found = 1
 		
 if not intro_found: xbmc.executebuiltin( "ReplaceWindow(Home)" )
